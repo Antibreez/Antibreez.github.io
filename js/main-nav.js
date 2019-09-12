2a6493ce43f249a52b4e3cdcbbab6337a813107c
Antibreez <kiyananton@gmail.com>
2019-09-13 01:35:12 +0300

    Add files via upload

---
+++ js/main-nav.js
@@ -0,0 +1,11 @@
+var menu = document.querySelector('.main-nav');
+var toggleButton = menu.querySelector('.main-nav__toggle');
+
+var onToggleClick = function (evt) {
+  evt.preventDefault();
+  menu.classList.toggle('main-nav--closed');
+  menu.classList.toggle('main-nav--opened');
+}
+
+toggleButton.addEventListener('click', onToggleClick);
+
