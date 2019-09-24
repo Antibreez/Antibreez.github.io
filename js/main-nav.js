e8548e2d9329518a68bb06e901363861a987404f
Anton Kiyan <kiyananton@gmail.com>
2019-09-24 21:45:07 +0300

    commit

--- js/main-nav.js
+++ js/main-nav.js
@@ -9,3 +9,27 @@ var onToggleClick = function (evt) {
 
 toggleButton.addEventListener('click', onToggleClick);
 
+var audio = document.querySelector('audio');
+
+console.log(audio);
+
+audio.addEventListener('loadedmetadata', function () {
+
+  var duration = audio.duration;
+  var date = new Date(duration * 1000);
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+
+  console.log(audio.duration);
+  console.log(minutes);
+
+  console.log(seconds);
+  console.log(minutes + ':' + (Math.floor(seconds / 10) === 0 ? ('0' + seconds) : seconds));
+
+});
+
+audio.addEventListener('timeupdate', function () {
+  console.log(audio.currentTime);
+})
+
+
